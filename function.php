d82d54623259255bbd6221d79c1607156a423382
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-04-29 18:52:19 +0530

    Update function.php

--- function.php
+++ function.php
@@ -1,24 +1,5 @@
 <?php
     function print_name(){
-        echo "
-        <table border='1'>
-            <tr>
-                <td>
-                    Name : 
-                </td>
-                <td>
-                    Kamal
-                </td>
-                <td>
-                    nimal
-                </td>
-            </tr>
-        </table>
-        ";
-
-
+        echo "Jehankandy";
     }
-
-
-
 ?>
