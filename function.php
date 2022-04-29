a68cb1b214e4a09f76a7f41565adb5f64359d3be
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-04-29 18:36:11 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,24 @@
+<?php
+    function print_name(){
+        echo "
+        <table border='1'>
+            <tr>
+                <td>
+                    Name : 
+                </td>
+                <td>
+                    Kamal
+                </td>
+                <td>
+                    nimal
+                </td>
+            </tr>
+        </table>
+        ";
+
+
+    }
+
+
+
+?>
